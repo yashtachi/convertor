ec5f8aad13485923b1fc29d934d5036051a463d4
yeshwanth <yashwanthmiryala1234@gmail.com>
2024-11-01 21:08:39 +0530

    added functionality and converted to statefulwidget

--- lib/currency_home.dart
+++ lib/currency_home.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class CurrencyHome extends StatelessWidget {
+class CurrencyHome extends StatefulWidget {
   const CurrencyHome({super.key});
+  @override
+  _CurrencyHomeState createState() => _CurrencyHomeState();
+}
+
+class _CurrencyHomeState extends State<CurrencyHome> {
+  double value = 0;
+  final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,13 +21,15 @@ class CurrencyHome extends StatelessWidget {
             style: TextStyle(
                 color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
           ),
-          const Padding(
-            padding: EdgeInsets.all(10.0),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
             child: TextField(
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              controller: textEditingController,
               cursorColor: Colors.white,
-              style: TextStyle(color: Colors.white),
+              style: const TextStyle(color: Colors.white),
               showCursor: true,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: 'Enter a value',
                 hintStyle: TextStyle(color: Color.fromARGB(255, 164, 163, 163)),
                 prefixIcon: Icon(
@@ -37,18 +47,31 @@ class CurrencyHome extends StatelessWidget {
           TextButton(
             style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(Colors.white),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0))),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
             ),
             onPressed: () {
-              // print("pressed");
+              setState(() {
+                value = double.parse(textEditingController.text) * 81;
+              });
             },
             child: const Text(
               'Convert',
-              style: TextStyle(color: Color.fromARGB(255, 82, 67, 219)),
-              selectionColor: Color.fromARGB(255, 235, 239, 245),
+              style: TextStyle(
+                color: Color.fromARGB(255, 82, 67, 219),
+              ),
             ),
           ),
+          Text(value.toString() + '   INR',
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+              ),
+              textAlign: TextAlign.center)
         ]),
       ),
     );
